7a21ac89e5dc6230b70ed8b7aa394ba017c6fedc
ytonykaku <juniormanhaesfilho@hotmail.com>
2024-11-18 12:41:27 -0300

    Finishing

--- main.c
+++ main.c
@@ -22,9 +22,20 @@ void *threadFunc(void *arg)
     }
 }
 
+void isClose(long counter, int guess){
+    int result = counter - guess;
+    if(result < 0) result = result * (-1);
+    if(result >= 50000) printf("\nMuito Frio\n");
+    else if(result <= 49999 && result >= 10000) printf("\nFrio\n");
+    else if(result <= 9999 && result >= 5000) printf("\nQuente\n");
+    else printf("\nMuito Quente\n");
+}
+
 int main(int argc, char *argv[]) {
     pthread_t t[THREADS];
     int s;
+    int guess1, guess2 = 0;
+
 
     /*sem_init(&mutex, 0, 1);*/
     for (int i = 0; i < THREADS; ++i) {
@@ -32,9 +43,30 @@ int main(int argc, char *argv[]) {
     }
 
     for (int i = 0; i < THREADS; ++i) {
-        s = pthread_join(t[i], NULL);//esperando elas terminares
+        s = pthread_join(t[i], NULL);//esperando elas terminarem
     }
+
+    for(int i = 0; i < 3; i++){
+        printf("Jogador 1, digite um número:\n");
+        scanf("%d", &guess1);
+        isClose(shared, guess1);
+        printf("Jogador 2, digite um número:\n");
+        scanf("%d", &guess2);  
+        isClose(shared, guess2);    
+    }
+
+    printf("Jogador 1, digite sua última jogada:\n");
+    scanf("%d", &guess1);
+    printf("Jogador 2, digite sua última jogada:\n");
+    scanf("%d", &guess2);    
 
     printf("compartilhada = %ld\n", shared);
 
+    guess1 = shared - guess1;
+    if(guess1 < 0) guess1 = guess1 * (-1);
+    guess2 = shared - guess2;
+    if(guess2 < 0) guess2 = guess2 * (-1);
+
+    if(guess1 < guess2) printf("Jogador 1 venceu");
+    else printf("Jogador 2 venceu");
 }
